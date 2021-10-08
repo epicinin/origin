f035b308f51ac74ba1855e23ce23fa563328fad1
dudupicinin@gmail.com <dudupicinin@gmail.com>
2021-10-08 03:31:43 +0000

    alterações na fato order

--- models/northwind/mart/dim_products.sql
+++ models/northwind/mart/dim_products.sql
@@ -1,23 +1,36 @@
 {{ config(materialized='table') }}
 
 with
-    staging as (
+    stg_products as (
         select *
         from {{ ref('stg_public_products')}}
+    )
+    , stg_categories as (
+        select *
+        from {{ ref('stg_public_categories')}}
+    )
+    , stg_suppliers as (
+        select *
+        from {{ ref('stg_public_suppliers')}}
     )
     , transformed as (
         select 
             row_number() over (order by product_id) as product_sk -- auto-incremental surrogate key
-            ,product_id
-            ,product_name
-            ,supplier_id
-            ,category_id
-            ,quantity_per_unit
-            ,unit_price
-            ,units_in_stock
-            ,units_on_order
-            ,reorder_level
-            ,discontinued
-            from staging
+            ,stg_products.product_id
+            ,stg_products.product_name
+            ,stg_products.supplier_id
+            ,stg_products.category_id
+            ,stg_products.quantity_per_unit
+            ,stg_products.unit_price
+            ,stg_products.units_in_stock
+            ,stg_products.units_on_order
+            ,stg_products.reorder_level
+            ,stg_products.discontinued
+            ,stg_categories.category_name
+            ,stg_suppliers.company_name
+            ,stg_suppliers.country
+            from stg_products
+            left join stg_categories on stg_products.category_id = stg_categories.category_id
+            left join stg_suppliers on stg_products.supplier_id = stg_suppliers.supplier_id
     )
 select * from transformed
